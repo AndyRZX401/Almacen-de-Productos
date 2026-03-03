2b7db65848e78abbbf9cded569eb89f81445b089
DELL 5490 <DELL 5490@ANDY>
2026-03-03 10:56:40 -0400

    Agregar archivos de proyecto.

---
+++ AlmacenProducto/almacen.cs
@@ -0,0 +1,44 @@
+﻿public class Almacen
+{
+    private List<Producto> productos;
+
+    public Almacen()
+    {
+        productos = new List<Producto>();
+    }
+
+    public bool AgregarProducto(Producto producto)
+    {
+        if (productos.Any(p => p.Codigo == producto.Codigo))
+            return false;
+
+        productos.Add(producto);
+        return true;
+    }
+
+    public Producto BuscarProducto(string codigo)
+    {
+        return productos.FirstOrDefault(p => p.Codigo == codigo);
+    }
+
+    public bool EliminarProducto(string codigo)
+    {
+        Producto p = BuscarProducto(codigo);
+        if (p != null)
+        {
+            productos.Remove(p);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Producto> ObtenerProductos()
+    {
+        return productos;
+    }
+
+    public int ObtenerCantidad()
+    {
+        return productos.Count;
+    }
+}
